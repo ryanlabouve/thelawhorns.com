01576da8d25c4168a263a11750601a7421e63f81
Ryan LaBouve <imryanlabouve@gmail.com>
2014-07-27 23:33:09 -0500

    code sprint

---
+++ app/js/main.js
@@ -0,0 +1,11 @@
+$(function() {
+	$('[data-trigger-modal="true"]').on("click", function(e) {
+		e.preventDefault();
+		$("#modal").fadeIn();
+	});
+
+	$('.close').on("click", function(e) {
+		e.preventDefault();
+		$("#modal").fadeOut();
+	});
+});
