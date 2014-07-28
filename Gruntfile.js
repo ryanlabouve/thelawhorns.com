f838e2eec0a3d1863752f955b5d944bdb483fb62
Ryan LaBouve <imryanlabouve@gmail.com>
2014-07-27 23:59:19 -0500

    Tweaking gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -280,10 +280,10 @@ module.exports = function (grunt) {
       dist: {
         files: [{
           src: [
-            '<%= yeoman.dist %>/js/**/*.js',
-            '<%= yeoman.dist %>/css/**/*.css',
-            '<%= yeoman.dist %>/img/**/*.{gif,jpg,jpeg,png,svg,webp}',
-            '<%= yeoman.dist %>/fonts/**/*.{eot*,otf,svg,ttf,woff}'
+            'js/**/*.js',
+            'css/**/*.css',
+            'img/**/*.{gif,jpg,jpeg,png,svg,webp}',
+            'fonts/**/*.{eot*,otf,svg,ttf,woff}'
           ]
         }]
       }
