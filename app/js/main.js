efb0a716f9b4d78f8bcfffb190ba30333b206771
Ryan LaBouve <imryanlabouve@gmail.com>
2014-07-31 22:24:07 -0500

    Add some simple parallax stuff to site

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,7 @@
-/* global $, skrollr */
+/* global $, skrollr, TweenMax, ScrollScene, ScrollMagic */
+
+var controller;
+
 $(function() {
 	$('[data-trigger-modal="true"]').on('click', function(e) {
 		e.preventDefault();
@@ -9,6 +12,25 @@ $(function() {
 		e.preventDefault();
 		$('#modal').fadeOut();
 	});
+
+	controller = new ScrollMagic();
+
+	// Logo Shrinking
+	var tween = TweenMax.to("#logo", 0.5, { scale: 0.5 });
+	var scene = new ScrollScene({triggerElement: "#nav1"})
+											.setTween(tween)
+											.addTo(controller);
+
+	// Keleigh Upside Down
+	var tween2 = TweenMax.fromTo("#g_1 img", 0.5,
+								{ "margin-top": "0px"},
+								{ "margin-top": "-200px"}
+							);
+	var scene2 = new ScrollScene({triggerElement: "#g_1", duration: 300})
+										.setTween(tween)
+										.addTo(controller);
+
+	// Use this to debug
+	// scene.addIndicators();
 });
 
-var s = skrollr.init();
